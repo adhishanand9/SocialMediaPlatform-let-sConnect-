12329c6f19b05bf82059bd88ecace36938ccbb3b
darsh2507 <darshp958@gmail.com>
2023-09-13 12:33:34 +1000

    retweet

--- routes/logout.js
+++ routes/logout.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
-// const bcrypt = require("bcrypt")
-// const User = require('../schemas/UserSchema');
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,8 +16,4 @@ router.get("/", (req, res, next) => {
         })
     }
 })
-
-
-
-
-module.exports = router;
+module.exports  = router;
